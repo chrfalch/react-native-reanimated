9cd1761e7190fc6aee0bebe042208dc2d732b09a
Christian Falch <christian@framx.no>
2019-04-25 08:17:50 +0200

    Moved conversion function into separate file

--- ios/REAJsiModule.mm
+++ ios/REAJsiModule.mm
@@ -10,8 +10,7 @@
 #include "REAJsiModule.h"
 #import <React/RCTBridge+Private.h>
 #import "REAModule.h"
-
-static NSDictionary *convertJSIObjectToNSDictionary(jsi::Runtime &runtime, const jsi::Object &value);
+#import "REAJsiUtilities.h"
 
 REAJsiModule::REAJsiModule(REAModule* reaModule)
 : reamodule_(reaModule) {}
@@ -55,7 +54,7 @@ jsi::Value REAJsiModule::get(jsi::Runtime &runtime, const jsi::PropNameID &name)
                                                                                    size_t count) -> jsi::Value {
             auto arg1 = &arguments[0];
             auto arg2 = &arguments[1];
-            [reamodule connectNodes:[NSNumber numberWithDouble:arg1->asNumber()] childTag:[NSNumber numberWithDouble:arg2->asNumber()]];
+            [reamodule connectNodes:[NSNumber numberWithDouble:(int)arg1->asNumber()] childTag:[NSNumber numberWithDouble:(int)arg2->asNumber()]];
             
             return jsi::Value::undefined();
         });
@@ -63,57 +62,5 @@ jsi::Value REAJsiModule::get(jsi::Runtime &runtime, const jsi::PropNameID &name)
     
     
     return jsi::Value::undefined();
-}
-
-static NSString *convertJSIStringToNSString(jsi::Runtime &runtime, const jsi::String &value) {
-    return [NSString stringWithUTF8String:value.utf8(runtime).c_str()];
-}
-
-static id convertJSIValueToObjCObject(jsi::Runtime &runtime, const jsi::Value &value);
-
-static NSDictionary *convertJSIObjectToNSDictionary(jsi::Runtime &runtime, const jsi::Object &value) {
-    jsi::Array propertyNames = value.getPropertyNames(runtime);
-    size_t size = propertyNames.size(runtime);
-    NSMutableDictionary *result = [NSMutableDictionary new];
-    for (size_t i = 0; i < size; i++) {
-        jsi::String name = propertyNames.getValueAtIndex(runtime, i).getString(runtime);
-        NSString *k = convertJSIStringToNSString(runtime, name);
-        id v = convertJSIValueToObjCObject(runtime, value.getProperty(runtime, name)) ?: (id)kCFNull;
-        result[k] = v;
-    }
-    return [result copy];
-}
-
-static NSArray *convertJSIArrayToNSArray(jsi::Runtime &runtime, const jsi::Array &value) {
-    size_t size = value.size(runtime);
-    NSMutableArray *result = [NSMutableArray new];
-    for (size_t i = 0; i < size; i++) {
-        [result addObject:convertJSIValueToObjCObject(runtime, value.getValueAtIndex(runtime, i)) ?: (id)kCFNull];
-    }
-    return [result copy];
-}
-
-static id convertJSIValueToObjCObject(jsi::Runtime &runtime, const jsi::Value &value) {
-    if (value.isUndefined() || value.isNull()) {
-        return nil;
-    }
-    if (value.isBool()) {
-        return @(value.getBool());
-    }
-    if (value.isNumber()) {
-        return @(value.getNumber());
-    }
-    if (value.isString()) {
-        return convertJSIStringToNSString(runtime, value.getString(runtime));
-    }
-    if (value.isObject()) {
-        jsi::Object o = value.getObject(runtime);
-        if (o.isArray(runtime)) {
-            return convertJSIArrayToNSArray(runtime, o.getArray(runtime));
-        }
-        return convertJSIObjectToNSDictionary(runtime, o);
-    }
-    
-    throw std::runtime_error("Unsupported jsi::jsi::Value kind");
 }
 
